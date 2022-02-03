e7e20316edfd3c361887fac9ac3ba8a3b5805d02
nebucaz <nebucaz@pageagent.com>
2022-02-03 16:47:07 +0000

    db-exchange

---
+++ db-exchange/init.sql
@@ -0,0 +1,5 @@
+CREATE USER "taler-exchange-httpd";
+CREATE USER "taler-exchange-wire";
+CREATE USER "taler-exchange-aggregator";
+CREATE USER "taler-exchange-closer";
+CREATE DATABASE "taler-exchange" OWNER "taler-exchange-httpd";
---
+++ exchange/docker-entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -x
+taler-exchange-httpd taler-exchange-dbinit
